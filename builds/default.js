de8ea7a52a23a031cb7cd05353be289439308956
Manolis Vrondakis <manolisvrondakis@gmail.com>
2023-01-04 12:10:13 +0000

    Update default.js

--- builds/default.js
+++ builds/default.js
@@ -7,7 +7,7 @@ module.exports = {
     based_on: {
         repo: "https://github.com/MarlinFirmware/Configurations/",
         path: "/config/examples/Creality/Ender-3/CrealityV427/",
-        stable_branch: "release-{{marlin_version}}",
+        stable_branch: "release-{{marlin_version}}", 
         nightly_branch: "bugfix-2.1.x"
     }
 };
